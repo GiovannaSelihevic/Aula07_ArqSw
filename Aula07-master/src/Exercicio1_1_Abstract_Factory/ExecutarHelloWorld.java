f4c49d6f9cdd11710fac76301ad4e6bff8d9e347
GiovannaSelihevic <36748070+GiovannaSelihevic@users.noreply.github.com>
2018-05-08 19:34:57 -0300

    Initial commit

---
+++ Aula07-master/src/Exercicio1_1_Abstract_Factory/ExecutarHelloWorld.java
@@ -0,0 +1,16 @@
+package Exercicio1_1_Abstract_Factory;
+
+/**
+ * 
+ * @author Giovanna Selihevic Marques - 81613657
+ *
+ */
+
+public class ExecutarHelloWorld {
+	//Metodo principal
+	public static void main(String[] args) {
+		HelloWorld cliente = new HelloWorld();
+		cliente.enviarHelloWorld();
+	}
+
+}
